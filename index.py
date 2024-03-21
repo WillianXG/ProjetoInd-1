8e49d97fbfea7ad48a08428ca4040fba8ace3eb9
Francisco <113522254+WillianXG@users.noreply.github.com>
2024-03-21 19:58:09 -0300

    att

--- index.py
+++ index.py
@@ -3,13 +3,63 @@ Candidatos = []
     
 for i in range(numCandidatos):
     nome = (input(f"Nome do Candidato {i +1}"))
-    notaE = (float(input(f"Nota da Entrevista do Candidato: ")))
-    notaT = (float(input(f"Nota do Teste Teórico do Candidato: ")))
-    notaP = (float(input(f"Nota do Teste Prático do Candidato: ")))
-    notaS = (float(input(f"Nota da Soft Skills do Candidato: ")))
+   
+    while True:
+        try:
+            notaE = (int(input(f"Nota da Entrevista do Candidato: ")))
+            if not 0 <= notaE <= 10:
+                raise ValueError("Nota inválida! Digite Novamente")
+        except ValueError as e:
+                print("Valor inválido")
+        else:
+                break
+        
+        
+    while True:
+        try:
+            notaT = (int(input(f"Nota do Teste Teórico do Candidato: ")))
+            
+            if not 0 <= notaT <= 10:
+                raise ValueError("Nota inválida! Digite Novamente")
+        except ValueError as e:
+                print("Valor inválido")
+        else:
+                break
     
+    
+    while True:
+        try:
+            notaP = (int(input(f"Nota do Teste Prático do Candidato: ")))
+            
+            if not 0 <= notaP <= 10:
+                raise ValueError("Nota inválida! Digite Novamente")
+        except ValueError as e:
+                print("Valor inválido")
+        else:
+                break
+    
+    while True:
+        try:
+            notaS = (int(input(f"Nota da Soft Skills do Candidato: ")))
+            
+            if not 0 <= notaS <= 10:
+                raise ValueError("Nota inválida! Digite Novamente")
+        except ValueError as e:
+                print("Valor inválido")
+        else:
+                break
+    
+    
+
     Candidatos.append((nome, notaE, notaT, notaP, notaS))
-    
+
+print("-------------------------------------------------------------------------")
+print("| Candidato\t\t| Resultado ")
+print("-------------------------------------------------------------------------")
+
 for cand in Candidatos:
-    nome, notaE, notaT, notaP, NotaS = cand
-    print("{nome}/t/t{notaE}")
+    nome, notaE, notaT, notaP, notaS = cand
+    notas_formatadas = f"e{notaE}_t{notaT}_p{notaP}_s{notaS}"
+    print(f"| {nome.ljust(15)}\t| {notas_formatadas.ljust(22)}")
+
+print("-------------------------------------------------------------------------")
